5fda5c4dd0654bdcf75744f0c862ad08fa455dc7
Cledeocir Marafão <cledeocirms96@gmail.com>
2026-02-20 04:38:57 -0300

    feat: eleventh component test

---
+++ src/components/AutomaticCounter.tsx
@@ -0,0 +1,19 @@
+import { animate, motion, useMotionValue, useTransform } from "motion/react"
+import { useEffect } from "react"
+
+export default function Counter() {
+    const count = useMotionValue(0)
+    const rounded = useTransform(() => Math.round(count.get()))
+
+    useEffect(() => {
+        const controls = animate(count, 1000, { duration: 5 })
+        return () => controls.stop()
+    }, [])
+
+    return <motion.pre style={text}>{rounded}</motion.pre>
+}
+
+const text = {
+    fontSize: 64,
+    color: "#8df0cc",
+}
